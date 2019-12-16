9a088d67161607ced15e67d60c6e7211209c7c79
Lee <Talamortis@gmail.com>
2019-12-16 18:35:37 +0000

    Travis fix
    Fixed Travis

--- src/game/Group.cpp
+++ src/game/Group.cpp
@@ -123,7 +123,7 @@ bool Group::Create(const uint64& guid, const char* name)
 
     if (!isBGGroup()) CharacterDatabase.CommitTransaction();
 
-    sScriptMgr.OnGroupCreated(leader->GetGroup(), leader);
+    sScriptMgr.OnGroupCreate(leader->GetGroup(), leader);
    
     // used by eluna
     sEluna->OnCreate(this, m_leaderGuid, m_groupType);
