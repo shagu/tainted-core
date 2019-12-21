631fa04002734999b4c1a34d25da28b330d0cab3
ReyDonovan <gennady.zamalaev@gmail.com>
2019-12-21 21:47:41 +0300

    Revert "Lib/OpenSSL: Update for OpenSSL (#107)"
    This reverts commit bd74eb235c02ada93e1203c52c8bede63417c42c.

--- src/shared/Auth/Hmac.cpp
+++ src/shared/Auth/Hmac.cpp
@@ -12,45 +12,30 @@
  * more details.
  *
  * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <https://www.gnu.org/licenses/>.
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "Auth/Hmac.h"
 #include "BigNumber.h"
 
-#if defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER < 0x10100000L
-HMAC_CTX* HMAC_CTX_new()
-{
-    HMAC_CTX *ctx = new HMAC_CTX();
-    HMAC_CTX_init(ctx);
-    return ctx;
-}
-
- void HMAC_CTX_free(HMAC_CTX* ctx)
-{
-    HMAC_CTX_cleanup(ctx);
-    delete ctx;
-}
-#endif
-
 HmacHash::HmacHash()
 {
     uint8 temp[SEED_KEY_SIZE] = { 0x38, 0xA7, 0x83, 0x15, 0xF8, 0x92, 0x25, 0x30, 0x71, 0x98, 0x67, 0xB1, 0x8C, 0x4, 0xE2, 0xAA };
     memcpy(&m_key, &temp, SEED_KEY_SIZE);
-    m_ctx = HMAC_CTX_new();
-    HMAC_Init_ex(m_ctx, &m_key, SEED_KEY_SIZE, EVP_sha1(), nullptr);
+    HMAC_CTX_init(&m_ctx);
+    HMAC_Init_ex(&m_ctx, &m_key, SEED_KEY_SIZE, EVP_sha1(), NULL);
 }
 
 HmacHash::HmacHash(uint32 /*len*/, uint8* /*seed*/)
 {
-    m_ctx = HMAC_CTX_new();
-    HMAC_Init_ex(m_ctx, &m_key, SEED_KEY_SIZE, EVP_sha1(), nullptr);
+    HMAC_CTX_init(&m_ctx);
+    HMAC_Init_ex(&m_ctx, &m_key, SEED_KEY_SIZE, EVP_sha1(), NULL);
     memset(m_digest, 0, sizeof(m_digest));
 }
 
 HmacHash::~HmacHash()
 {
-    HMAC_CTX_free(m_ctx);
+    HMAC_CTX_cleanup(&m_ctx);
 }
 
 void HmacHash::UpdateBigNumber(BigNumber* bn)
@@ -58,24 +43,24 @@ void HmacHash::UpdateBigNumber(BigNumber* bn)
     UpdateData(bn->AsByteArray(), bn->GetNumBytes());
 }
 
-void HmacHash::UpdateData(uint8 const* data, int length)
+void HmacHash::UpdateData(const uint8* data, int length)
 {
-    HMAC_Update(m_ctx, data, length);
+    HMAC_Update(&m_ctx, data, length);
 }
 
-void HmacHash::UpdateData(std::string const& str)
+void HmacHash::UpdateData(const std::string& str)
 {
-    UpdateData(reinterpret_cast<uint8 const*>(str.c_str()), str.length());
+    UpdateData((uint8 const*)str.c_str(), str.length());
 }
 
 void HmacHash::Initialize()
 {
-    HMAC_Init_ex(m_ctx, &m_key, SEED_KEY_SIZE, EVP_sha1(), nullptr);
+    HMAC_Init_ex(&m_ctx, &m_key, SEED_KEY_SIZE, EVP_sha1(), NULL);
 }
 
 void HmacHash::Finalize()
 {
     uint32 length = 0;
-    HMAC_Final(m_ctx, m_digest, &length);
+    HMAC_Final(&m_ctx, m_digest, &length);
     ASSERT(length == SHA_DIGEST_LENGTH);
 }
