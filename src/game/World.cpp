b92468b65dad65f7a7ef3e9ba2d9b3ef24d2ebfc
Lee <talamortis@gmail.com>
2020-01-19 10:05:59 +0000

    fixed compile on windows
    added missing include for importing module sql files

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -68,6 +68,7 @@
 #include "M2Stores.h"
 
 #include <ace/Dirent.h>
+#include <ace/OS_NS_sys_stat.h>
 
 INSTANTIATE_SINGLETON_1(World);
 
@@ -1255,7 +1256,9 @@ void World::LoadModSQLUpdates()
             {
                 sLog.outFatal("directory error %s: %s", path.c_str(), strerror(errno));
                 continue;
-            }
+			}
+
+
             switch (stat_buf.st_mode & S_IFMT)
             {
             case S_IFDIR://is directory?
