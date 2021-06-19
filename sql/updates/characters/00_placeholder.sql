d7c7a149ed57931088738b63e7a2d6e2f1e9c1e5
shagu <ericmauser@googlemail.com>
2021-06-19 20:20:31 +0200

    sql: fix missing semicolon in sql placeholders

--- sql/updates/characters/00_placeholder.sql
+++ sql/updates/characters/00_placeholder.sql
@@ -1,3 +1,3 @@
 -- Add a title to indicate the update process. This is also a placeholder to
 -- prevent from errors, while updating after a release when folders are still empty.
-SELECT '' AS 'Installing Updates...'
+SELECT '' AS 'Installing Updates...';
